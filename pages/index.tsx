6ae5ec50e4bf05bed59216d68294d2255445e915
Ferruh <ferruhcihan20@gmail.com>
2022-03-25 20:53:00 +0300

    feat:update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import { Toolbar } from "../components/toolbar";
 
 const Home: NextPage = () => {
-  return <h1 className="text-3xl font-bold underline bg-red">Hello world!</h1>;
+  return (
+    <div>
+      <Toolbar />
+      <h1 className="text-3xl font-bold underline bg-red">Hello world!</h1>
+    </div>
+  );
 };
 
 export default Home;
