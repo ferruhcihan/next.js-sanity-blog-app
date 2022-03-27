68c6cdaaf3d4afc52edb81afb657b40cc6abcbef
Ferruh <ferruhcihan20@gmail.com>
2022-03-26 20:00:00 -0600

    feat:update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,18 +2,25 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { Toolbar } from "../components/toolbar";
+import { useRouter } from "next/router";
 
 interface IHome {
   posts: any[];
 }
 
 export default function Home({ posts }: IHome) {
+  const router = useRouter();
   console.log("posts", posts);
 
   return (
     <div>
       <Toolbar />
-      <h1 className="text-3xl font-bold underline bg-red">Hello world!</h1>
+      <h1
+        className="text-3xl font-bold text-center underline cursor-pointer"
+        onClick={() => router.push(`/post/${posts[0].slug.current}`)}
+      >
+        {posts[0].title}
+      </h1>
     </div>
   );
 }
@@ -23,7 +30,7 @@ export const getServerSideProps = async (pageContext: any) => {
   const url = `https://mnlo2t4u.api.sanity.io/v1/data/query/production?query=${query}`;
   const result = await fetch(url).then((res) => res.json());
 
-  if (!result.result || result.result.length) {
+  if (!result.result || !result.result.length) {
     return {
       props: {
         posts: [],
